8719ad64b919f9d70b11a6c46a8fe745a2e414d0
Ajith Pattammattel <ajith.pattammattel@gmail.com>
2020-11-18 13:11:53 -0500

    correlation plots added

--- try.py
+++ try.py
@@ -19,35 +19,3 @@ img = tf.imread('test_stack.tiff')
 
 img1 = img[10].flatten()
 img2 = img[50].flatten()
-
-app = QtGui.QApplication([])
-mw = QtGui.QMainWindow()
-mw.resize(800,800)
-view = pg.GraphicsLayoutWidget()  ## GraphicsView with GraphicsLayout inserted by default
-mw.setCentralWidget(view)
-mw.show()
-mw.setWindowTitle('pyqtgraph example: ScatterPlot')
-
-## create four areas to add plots
-w1 = view.addPlot()
-
-## There are a few different ways we can draw scatter plots; each is optimized for different types of data:
-
-
-## 1) All spots identical and transform-invariant (top-left plot).
-## In this case we can get a huge performance boost by pre-rendering the spot
-## image and just drawing that image repeatedly.
-
-
-s1 = pg.ScatterPlotItem(size=10, pen=pg.mkPen(None), brush=pg.mkBrush(0, 255, 0, 120))
-pos = np.array((img1,img2))
-s1.setData(spots)
-w1.addItem(s1)
-
-print(np.shape(spots))
-
-## Start Qt event loop unless running in interactive mode.
-if __name__ == '__main__':
-    import sys
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
